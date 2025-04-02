02ce3fedf8453c51e66ede40a59137f9d04c647a
Kevin Johnson <kevin@inventures.cl>
2025-04-02 10:01:00 -0300

    Refactor: simplify deforestation analysis module title counter

--- monbo-front/src/app/[locale]/deforestation-analysis/PageTitle.tsx
+++ monbo-front/src/app/[locale]/deforestation-analysis/PageTitle.tsx
@@ -1,48 +1,26 @@
 "use client";
 
-import { DataContext } from "@/context/DataContext";
-import { useContext, useMemo, useState } from "react";
+import { useState } from "react";
 import { Text } from "@/components/reusable/Text";
 import { useTranslation } from "react-i18next";
 import { formatNumber } from "@/utils/numbers";
-import { FarmValidationStatus } from "@/interfaces/PolygonValidation";
 import { Box, IconButton } from "@mui/material";
 import InfoOutlined from "@mui/icons-material/InfoOutlined";
 import { MapsDetailsModal } from "./MapsDetailsModal";
+import { useVisibleDataForDeforestationPage } from "@/hooks/useVisibleDataForDeforestationPage";
+
 export const PageTitle: React.FC = () => {
-  const {
-    farmsData,
-    polygonsValidationResults,
-    deforestationAnalysisParams: { polygonsSubset },
-  } = useContext(DataContext);
   const { t } = useTranslation();
+  const { farmsData } = useVisibleDataForDeforestationPage();
 
   const [open, setOpen] = useState(false);
-
-  const farmsAmount = useMemo(() => {
-    if (!farmsData) return 0;
-
-    if (!polygonsSubset || polygonsSubset === "all") {
-      return farmsData.length;
-    }
-
-    if (polygonsSubset === "valid") {
-      // Count the farms that were valid at the polygons validation stage
-      return farmsData.filter(
-        (f) =>
-          polygonsValidationResults?.farmResults.find(
-            ({ farmId }) => farmId === f.id
-          )?.status !== FarmValidationStatus.NOT_VALID
-      ).length;
-    }
-    return 0;
-  }, [farmsData, polygonsValidationResults, polygonsSubset]);
 
   return (
     <>
       <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
         <Text variant="h3" bold>
-          {t("deforestationAnalysis:title")} ({formatNumber(farmsAmount)})
+          {t("deforestationAnalysis:title")} (
+          {formatNumber(farmsData?.length ?? 0)})
         </Text>
         <IconButton onClick={() => setOpen(true)}>
           <InfoOutlined />
