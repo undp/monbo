9dd0e617e58eb50f489c3e9b0d6b65b6e5b814d5
Kevin Johnson <kevin@inventures.cl>
2025-04-02 16:14:01 -0300

    Feat: enable the navigation to the 3rd module at homepage

--- monbo-front/src/components/page/home/ReportCreationModuleCard.tsx
+++ monbo-front/src/components/page/home/ReportCreationModuleCard.tsx
@@ -1,12 +1,18 @@
+"use client";
+
 import { ModuleCard } from "@/components/page/home/ModuleCard";
+import { useModuleRouter } from "@/hooks/useModuleRouter";
 
 export const ReportCreationModuleCard: React.FC = () => {
+  const goToModule = useModuleRouter("/report-generation");
+
   return (
     <ModuleCard
       step={3}
       textNamespace="home:report"
       color="#8C1BB5"
       imgSrc="/images/Step4Img.svg"
+      onPress={goToModule}
     />
   );
 };
